0cb4d0ffb6a09446dee31baedc2ab427d78a398f
AWS ParallelCluster user <ec2-user@ip-10-0-2-37.ap-southeast-1.compute.internal>
2021-10-29 07:32:12 +0000

    New Scripts

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -6,21 +6,18 @@
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --cpus-per-task=1	# number of processors per task
-#SBATCH -J "rse"   # job name
+#SBATCH -J "rplhighpass"   # job name
 
 ## /SBATCH -p general # partition (queue)
-#SBATCH -o rse-slurm.%N.%j.out # STDOUT
-#SBATCH -e rse-slurm.%N.%j.err # STDERR
+#SBATCH -o rplhighpass-slurm.%N.%j.out # STDOUT
+#SBATCH -e rplhighpass-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 python -u -c "import PyHipp as pyh; \
 import time; \
-import os; \
-t0 = time.time(); \
-print(time.localtime()); \
-os.chdir('sessioneye'); \
-pyh.RPLSplit(SkipLFP=False, SkipHighPass=False); \
-print(time.localtime()); \
-print(time.time()-t0);"
-
-aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:018084650241:awsnotify --message "RSEJobDone"
+pyh.RPLHighPass(saveLevel=1); \
+from PyHipp import mountain_batch; \
+mountain_batch.mountain_batch(); \
+from PyHipp import export_mountain_cells; \
+export_mountain_cells.export_mountain_cells(); \
+print(time.localtime());"
